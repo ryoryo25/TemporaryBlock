c7a49d09fe6391b8a0d87793bf34f3c1ce36d1a7
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-11-26 14:30:02 +0900

    register recipe of temporary block placer

--- src/main/java/ryoryo/temporaryblock/proxy/CommonProxy.java
+++ src/main/java/ryoryo/temporaryblock/proxy/CommonProxy.java
@@ -1,10 +1,14 @@
 package ryoryo.temporaryblock.proxy;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import ryoryo.temporaryblock.TemporaryBlock;
 import ryoryo.temporaryblock.block.ModBlocks;
 import ryoryo.temporaryblock.handler.ChainDestructionHandler;
 import ryoryo.temporaryblock.handler.TemporaryBlockReplaceHandler;
@@ -17,6 +21,8 @@ public class CommonProxy {
 	}
 
 	public void init(FMLInitializationEvent event) {
+		TemporaryBlock.REGISTER.addRecipe("temporary_block_placer", new ItemStack(ModItems.ITEM_TEMPORARY_BLOCK_PLACER), "sCs", "CsC", "sCs", 's', Items.STICK, 'C', Blocks.COBBLESTONE);
+
 		MinecraftForge.EVENT_BUS.register(new TemporaryBlockReplaceHandler());
 		MinecraftForge.EVENT_BUS.register(new ChainDestructionHandler());
 	}
