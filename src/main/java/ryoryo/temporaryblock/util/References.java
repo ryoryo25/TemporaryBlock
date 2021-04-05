15580245754efae01377726f941713fba54f0a2f
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2021-04-06 04:52:06 +0900

    update version of PolishedLib to 1.1.0

--- src/main/java/ryoryo/temporaryblock/util/References.java
+++ src/main/java/ryoryo/temporaryblock/util/References.java
@@ -10,7 +10,7 @@ public class References {
 	public static final String MOD_VERSION = MOD_VERSION_MAJOR + "." + MOD_VERSION_MINOR + "." + MOD_VERSION_PATCH;
 
 	public static final String MOD_DEPENDENCIES = "required-after:forge@[14.23.5.2768,);"
-			//			+ "required-after:polishedlib@[1.0.3,);";
+			//			+ "required-after:polishedlib@[1.1.0,);";
 			+ "required-after:polishedlib;";
 	public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.12.2]";
 	public static final String MOD_GUI_FACTORY = "ryoryo.temporaryblock.config.GuiFactoryModConfig";
